557397271f0d56781df0b0eb68caa80afbfd5352
Brittany Gollins and Christabel Escarez <christabel@>
2018-09-17 19:56:32 -0700

    set up amusement ride options and -if else- to highlight rides that are rated as meant for shorter folks or taller folks

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,15 @@
 
 // front end side
 $(document).ready(function() {
-  
+  $("form#heightmeter").submit(function(event) {
+
+    var heightInput = parseInt($("input#height").val());
+
+    if (heightInput < 36) {
+      $(".tots").addClass("highlight");
+    } else {
+      $(".big").addClass("lowlight");
+    }
+    event.preventDefault();
+  });
 });
